386c72dc85784f1fd5131df1989356d9f5022455
sameh <smta55@gmail.com>
2024-03-30 01:43:21 +0200

    ReadMe & app instalation command

---
+++ app/Console/Commands/installApp.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class installApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'install:app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the application by running migrations and seeding the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+        $this->call('db:seed');
+        $this->call('serve');
+        
+        $this->info('Application installed successfully!');
+    }
+}
