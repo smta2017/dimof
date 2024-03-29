5022eea4909e3a18e1441c38ea341651c4223b5f
sameh <smta55@gmail.com>
2024-03-30 01:42:16 +0200

    Unit test

--- app/Http/Requests/API/CreatePostAPIRequest.php
+++ app/Http/Requests/API/CreatePostAPIRequest.php
@@ -3,9 +3,9 @@
 namespace App\Http\Requests\API;
 
 use App\Models\Post;
-use InfyOm\Generator\Request\APIRequest;
+use Illuminate\Foundation\Http\FormRequest;
 
-class CreatePostAPIRequest extends APIRequest
+class CreatePostAPIRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/API/UpdatePostAPIRequest.php
+++ app/Http/Requests/API/UpdatePostAPIRequest.php
@@ -3,9 +3,9 @@
 namespace App\Http\Requests\API;
 
 use App\Models\Post;
-use InfyOm\Generator\Request\APIRequest;
+use Illuminate\Foundation\Http\FormRequest;
 
-class UpdatePostAPIRequest extends APIRequest
+class UpdatePostAPIRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
